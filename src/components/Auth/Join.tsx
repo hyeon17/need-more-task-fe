48da451b7f1c9a907a494f895b413fcfce0f2a66
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-02 00:10:35 +0900

    feat, refactor: 회원가입, 로그인 페이지의 input, password input 컴포넌트화
    1. 로그인 페이지 완료
    2. 회원가입, 로그인 페이지 AuthInput, PasswordInput 컴포넌트로 분리

--- src/components/Auth/Join.tsx
+++ src/components/Auth/Join.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import * as A from '@/styles/auth.styles';
 import TitleLabel from './TitleLabel';
-import FooterLabel from './FooterLabel';
+import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
 import PasswordInput from './PasswordInput';
-import { inputProps } from '@/components/Auth/Join';
 import AuthInput from './AuthInput';
+
+export const inputProps = {
+  variant: 'flushed',
+  borderColor: 'outlineColor',
+  focusBorderColor: 'inputFocusColor',
+};
 
 function Join() {
   const [show, setShow] = useState(false);
@@ -14,16 +19,22 @@ function Join() {
     <A.Container>
       <TitleLabel title="로그인" />
       <A.BodyWrapper>
+        {/* 이름 */}
+        <AuthInput label="이름" placeholder="이름을 입력하세요" inputProps={inputProps} />
         {/* 이메일 */}
         <AuthInput label="이메일" placeholder="이메일을 입력하세요" inputProps={inputProps} />
+        {/* 닉네임 */}
+        <AuthInput label="닉네임" placeholder="닉네을 입력하세요" inputProps={inputProps} />
+
         {/* 비밀번호 */}
         <PasswordInput label="비밀번호" show={show} handleClick={handleClick} inputProps={inputProps} />
+        {/* 비밀번호 확인 */}
+        <PasswordInput label="비밀번호 확인" show={show} handleClick={handleClick} inputProps={inputProps} />
 
         <A.ConfirmButton colorScheme="teal" size="md">
-          로그인
+          회원가입
         </A.ConfirmButton>
       </A.BodyWrapper>
-      <FooterLabel />
     </A.Container>
   );
 }
---
+++ src/pages/join.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '@/components/Layout';
+import Join from '@/components/Auth/Join';
+
+function JoinPage() {
+  return (
+    <Layout>
+      <Join />
+    </Layout>
+  );
+}
+
+export default JoinPage;
