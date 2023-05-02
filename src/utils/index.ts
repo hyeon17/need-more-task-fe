e3a746fd74479266e73fefab3c670631815286b0
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-02 20:48:34 +0900

    feat: 입사연도 계산 utils 추가, 부서 select를 위한 option utils 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,37 @@
+import dayjs from 'dayjs';
+
 export function classnames(...args: string[]) {
-  return args.join(" ");
+  return args.join(' ');
 }
 
 export const taskTitle = (title: string) => {
-  return title.length > 30
-    ? title.substring(0, 30).concat(" ..." + "더보기")
-    : title;
+  return title.length > 30 ? title.substring(0, 30).concat(' ...' + '더보기') : title;
+};
+
+export const teamOptions = [
+  { value: '개발', label: '개발' },
+  { value: '인사', label: '인사' },
+  { value: '경영', label: '경영' },
+  { value: '무역', label: '무역' },
+  { value: '영업', label: '영업' },
+  { value: '서비스', label: '서비스' },
+  { value: '생산', label: '생산' },
+  { value: '교육', label: '교육' },
+  { value: '마케팅', label: '마케팅' },
+  { value: '기타', label: '기타' },
+];
+
+export const getJoinCompanyYear = () => {
+  const thisYear = dayjs().year();
+  return [...Array(thisYear - 1899)].map((_v, i) => {
+    if (i === 0)
+      return {
+        label: '연 선택',
+        value: '',
+      };
+    return {
+      label: thisYear - i,
+      value: thisYear - i,
+    };
+  });
 };
