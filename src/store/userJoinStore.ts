cc3ea01ec1d91b71cb498f1c7a3726d1f77ea5d8
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-02 20:44:40 +0900

    feat: 회원가입 user 정보 zustand 전역으로 저장

---
+++ src/store/userJoinStore.ts
@@ -0,0 +1,30 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+export interface IJoinData {
+  department?: string;
+  joinCompanyYear?: number;
+  fullname?: string;
+  email?: string;
+  nickname?: string;
+  password?: string;
+  confirmPassword?: string;
+  imagePath?: string;
+  phone?: string;
+}
+
+interface IUserJoinInfoStore {
+  me: IJoinData | null;
+  onResetSignup: () => void;
+  onSaveSignup: (signupData: IJoinData) => void;
+}
+
+const userJoinStore = create<IUserJoinInfoStore>()(
+  devtools((set) => ({
+    me: null,
+    onSaveSignup: (signupData) => set((state) => ({ me: { ...state.me, ...signupData } })),
+    onResetSignup: () => set(() => ({ me: null })),
+  })),
+);
+
+export const useUserJoinStore = () => userJoinStore((state) => state);
