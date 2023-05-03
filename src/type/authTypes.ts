f7f4ef5accab11a967b0c0e4cccf6c1d92e4f153
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-04 02:50:07 +0900

    feat: auth 관련 type 추가

---
+++ src/type/authTypes.ts
@@ -0,0 +1,9 @@
+export interface IJoin {
+  email?: string;
+  password?: string;
+  password_check?: string;
+  phone?: string;
+  fullname?: string;
+  department?: string;
+  joinCompanyYear?: string;
+}
