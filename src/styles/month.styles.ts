ba6a65fc275431076776280ed77d8cc093660e8a
hyeon17 <janghyeon8725@naver.com>
2023-05-03 21:36:46 +0900

    feat: 다음달, 이전달 이동 버튼 추가

--- src/styles/month.styles.ts
+++ src/styles/month.styles.ts
@@ -2,7 +2,9 @@ import styled from '@emotion/styled';
 import { Button, Divider } from '@chakra-ui/react';
 
 export const Container = styled.div`
-  width: 1000px;
-  display: flex;
-  flex-direction: column;
+width:1000px;
+
+
+
+
 `;
