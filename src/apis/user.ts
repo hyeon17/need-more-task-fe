7f8f6f6ac28e5d3181bc525ee5cf1044182dbd26
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-04 02:49:39 +0900

    feat: auth 관련 Tanstack-Query custom hook 추가

---
+++ src/apis/user.ts
@@ -0,0 +1,15 @@
+import { IJoin } from '@/type/authTypes';
+import { useMutation, UseMutationOptions, useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { AxiosError, AxiosResponse } from 'axios';
+import { useRouter } from 'next/router';
+import { axiosInstance } from '@/apis/configs';
+
+export const joinAPI = (options?: UseMutationOptions<AxiosResponse<string>, AxiosError, IJoin>) => {
+  const router = useRouter();
+  const queryKey = `/join`;
+  const queryFn = (data: IJoin) => axiosInstance.post(queryKey, data).then((res) => res.data);
+
+  // const onSuccess = () => router.push('/join/complete');
+  const onSuccess = (data: any) => console.log('response>>>', data);
+  return useMutation([queryKey], queryFn, { onSuccess, ...options });
+};
