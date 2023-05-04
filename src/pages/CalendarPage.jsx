ebcb3d908e77c969dd1fc62127bad03171c7288e
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-04 20:54:29 +0900

    feat: Calendar 페이지 CommonHeader 추가

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,18 +1,14 @@
-import React from 'react'
+import React from 'react';
 import Layout from '@/components/Layout';
 import dynamic from 'next/dynamic';
 
-
-const ComponentsWithNoSSR = dynamic(
-  () => import('@/components/Calendar/Calendar'),
-  { ssr: false },
-);
+const ComponentsWithNoSSR = dynamic(() => import('@/components/Calendar/Calendar'), { ssr: false });
 
 function CalendarPage() {
   return (
-      <Layout>
-        <ComponentsWithNoSSR />
-      </Layout>
+    <Layout hasHeader>
+      <ComponentsWithNoSSR />
+    </Layout>
   );
 }
 
