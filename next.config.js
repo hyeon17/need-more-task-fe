6779a39a139649f94b8ee9d5992cb403a93f506e
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-04 13:03:10 +0900

    feat: next.config.js - image domain 추가

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'www.gravatar.com'],
   },
   experimental: {
     esmExternals: false,
