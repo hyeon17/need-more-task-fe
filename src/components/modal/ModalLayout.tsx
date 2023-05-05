5ba4c7c99680e93b5f47ffdee04b91404636cb15
BeeMOre32 <windowssart01@gmail.com>
2023-05-05 15:21:46 +0900

    refact: 의도치 않게 Layout 에 motion variants 들어가지 않았던 코드 수정

--- src/components/modal/ModalLayout.tsx
+++ src/components/modal/ModalLayout.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import CreateTask from '@/components/modal/CreateTask';
 import { Modal, ModalOverlay } from '@chakra-ui/react';
-import { motion } from 'framer-motion';
+import { motion, Variants } from 'framer-motion';
 import { useModalState } from '@/store/modalStore';
 import TaskOverview from '@/components/modal/TaskOverview';
+
+const overlayVariants: Variants = {
+  initial: { opacity: 0, backgroundColor: 'rgba(0, 0, 0, 0)' },
+  animate: { backgroundColor: 'rgba(0, 0, 0, 0.7)', opacity: 1 },
+  exit: { opacity: 0, backgroundColor: 'rgba(0, 0, 0, 0)' },
+};
 
 function ModalLayout() {
   const { modal, id, onCloseModal } = useModalState();
   return (
     <Modal isOpen={!!modal} onClose={onCloseModal} key={id} motionPreset="slideInBottom" isCentered>
-      <ModalOverlay as={motion.div} initial="initial" animate="animate" exit="exit" backdropFilter="blur(5px)" />
+      <ModalOverlay
+        as={motion.div}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={overlayVariants}
+        backdropFilter="blur(5px)"
+      />
       {modal === 'create' && <CreateTask />}
       {modal === 'overview' && <TaskOverview />}
       {modal === null && <div>Modal is null</div>}
