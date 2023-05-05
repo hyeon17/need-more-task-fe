1c62a692fc7e53915423b1279f64da7771475afd
BeeMOre32 <windowssart01@gmail.com>
2023-05-05 15:20:56 +0900

    feat: task를 받아오는 함수 제작

---
+++ src/apis/task.ts
@@ -0,0 +1,9 @@
+import { KanbanBoardDataInterface, TaskData } from '@/apis/kanban';
+import { axiosInstance } from '@/apis/configs';
+
+export type TaskDetailData = KanbanBoardDataInterface;
+
+export const getTaskDetail = async (taskID: number) => {
+  const res = await axiosInstance.get<TaskDetailData>(`/task/${taskID}`);
+  return res.data;
+};
