d2c3e7c3cc2f3a3ed1e233b24ecc3f1ddd78440a
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-07 04:47:55 +0900

    feat: getTaskProgressAPI custom api 추가

---
+++ src/apis/dashboard.ts
@@ -0,0 +1,22 @@
+import { IJoin, ILogin, IUpdateProfile, IValidatePassword } from '@/type/authTypes';
+import { useMutation, UseMutationOptions, useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { AxiosError, AxiosResponse } from 'axios';
+import { useRouter } from 'next/router';
+import { axiosInstance, axiosWithToken } from '@/apis/configs';
+
+// export const updateUserInfoAPI = (
+//   userId: string,
+//   options?: UseMutationOptions<AxiosResponse<string>, AxiosError, IUpdateProfile>,
+// ) => {
+//   const queryKey = `/user/${userId}`;
+//   const queryFn = (data: IUpdateProfile) => axiosWithToken.put(queryKey, data).then((res) => res.data);
+
+//   return useMutation([queryKey], queryFn, { ...options });
+// };
+
+export const getTaskProgressAPI = (options?: UseQueryOptions<AxiosResponse<any>, AxiosError, any, string[]>) => {
+  const queryKey = `/progress`;
+  const queryFn = () => axiosWithToken.get(queryKey).then((res) => res.data);
+
+  return useQuery([queryKey], queryFn, { ...options });
+};
