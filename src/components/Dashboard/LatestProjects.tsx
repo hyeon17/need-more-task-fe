5e9094647882b66a706d09bb3dbd959ad41d5a8d
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-08 05:58:53 +0900

    feat: LatestProjects - 모든 Tasks 버튼 추가

--- src/components/Dashboard/LatestProjects.tsx
+++ src/components/Dashboard/LatestProjects.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import * as D from '@/styles/dashboard.styles';
+import EmptyProjects from '@/components/Dashboard/EmptyProjects';
+import LatestProjectsList from '@/components/Dashboard/LatestProjectsList';
+import { Button } from '@chakra-ui/react';
 
 function LatestProjects() {
   return (
     <D.LatestProjectContainer>
       <D.DashboardH5>
-        <h5>Latest Projects</h5>
+        <div>
+          <h5>Latest Projects</h5>
+          <Button color="primary" variant="outline">
+            모든 Tasks
+          </Button>
+        </div>
       </D.DashboardH5>
+
+      {/* empty projects lottie */}
+      {/* <EmptyProjects /> */}
+      <LatestProjectsList />
     </D.LatestProjectContainer>
   );
 }
