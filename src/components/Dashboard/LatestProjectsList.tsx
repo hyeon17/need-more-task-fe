c94755951695ec0e8617f4de6961b19527cc952d
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-09 20:01:07 +0900

    feat: LatestProjectsList - formattedDate utils 함수 추가

--- src/components/Dashboard/LatestProjectsList.tsx
+++ src/components/Dashboard/LatestProjectsList.tsx
@@ -5,6 +5,7 @@ import CommonAvatar from '@/components/CommonAvatar/CommonAvatar';
 import { getLatestProjectsAPI } from '@/apis/dashboard';
 import EmptyProjects from '@/components/Dashboard/EmptyProjects';
 import CommonSpinner from '@/components/common/CommonSpinner';
+import { formattedDate } from '@/utils';
 
 function LatestProjectsList() {
   const { data: latestProjectsData, isLoading } = getLatestProjectsAPI();
@@ -16,8 +17,11 @@ function LatestProjectsList() {
       {latestProjectsData?.data.length === 0 && <EmptyProjects />}
       {latestProjectsData && latestProjectsData.data.length > 0
         ? latestProjectsData.data.map((data: any) => {
+            // console.log('data>>', data);
+
             const { taskId, taskOwner, createdAt, startAt, endAt, title, desc, assignee, priority, progress } = data;
-            const { userId, profileImageUrl } = assignee;
+            // console.log('assignee>>', assignee);
+
             return (
               <D.LatestProjectsListWrapper key={`latestProject${taskId}`}>
                 <D.ProjectsListBodyWrapper>
@@ -37,7 +41,7 @@ function LatestProjectsList() {
                 <D.ProjectListFooterWrapper>
                   <footer>
                     <span>Assignee {assignee?.length}</span>
-                    <span>{createdAt}</span>
+                    <span>{formattedDate(createdAt)}</span>
                   </footer>
                 </D.ProjectListFooterWrapper>
               </D.LatestProjectsListWrapper>
