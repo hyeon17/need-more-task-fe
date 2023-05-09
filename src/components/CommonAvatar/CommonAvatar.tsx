9f0c26a67004e6471813e4f0c16ed2ad16e8d769
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-09 23:32:41 +0900

    feat: CommonAvatar.tsx - router.push(`/profile/${userId}`) 프로필 페이지로 이동

--- src/components/CommonAvatar/CommonAvatar.tsx
+++ src/components/CommonAvatar/CommonAvatar.tsx
@@ -1,5 +1,7 @@
 import { Avatar, AvatarGroup } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 interface IAssignee {
@@ -20,14 +22,18 @@ function CommonAvatar({
   max = 2,
   src = 'https://www.gravatar.com/avatar?d=mp&f=y',
 }: ICommonAvatar) {
+  const router = useRouter();
   return (
     <AvatarGroup size={size} max={max}>
       {assignee?.map((user: IAssignee) => {
         const { profileImageUrl, userId } = user;
         return (
-          <Link href={`/profile/${userId}`} key={`userId${userId}`}>
-            <Avatar src={profileImageUrl ? profileImageUrl : src} />
-          </Link>
+          <StyledAvatar
+            src={profileImageUrl ? profileImageUrl : src}
+            key={`userId${userId}`}
+            onClick={() => router.push(`/profile/${userId}`)}
+            style={{ cursor: 'pointer' }}
+          />
         );
       })}
     </AvatarGroup>
@@ -35,3 +41,7 @@ function CommonAvatar({
 }
 
 export default CommonAvatar;
+
+export const StyledAvatar = styled(Avatar)`
+  cursor: pointer;
+`;
