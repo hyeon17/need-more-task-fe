614bf7d5a5c52dad249d469916f6a015bbaa15c2
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-09 13:19:18 +0900

    feat: 실제 backend endpoint 추가

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['localhost', 'www.gravatar.com', 'ec2-52-78-64-143.ap-northeast-2.compute.amazonaws.com:8080'],
+    domains: ['localhost', 'www.gravatar.com', 'ec2-43-200-108-182.ap-northeast-2.compute.amazonaws.com:8080'],
   },
   experimental: {
     esmExternals: false,
