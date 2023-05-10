2822df8cc4dee6591c95994aab608c2707d83f2c
hyeon17 <janghyeon8725@naver.com>
2023-05-11 02:26:04 +0900

    feat: 타입 추가, 로딩 시 스켈레톤ui 추가

--- src/components/OverView/Content.tsx
+++ src/components/OverView/Content.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import * as S from '@/styles/overview.styles';
 import { useOverViewState } from '@/store/overViewStore';
+import { OverViewProps, TaskOverviewProps } from '@/type/componentProps';
 
-function Content({ content }: any) {
+function Content({ content, isLoading }: OverViewProps) {
   const { displayedData } = useOverViewState();
   const data = displayedData ? displayedData : content;
   return (
     <S.OverViewContent>
-      {data.map((event: any) => {
-        return (
-          <S.Cards variant={'outline'} key={event.id}>
-            <S.CardWrapper>
-              <S.CardTitle>{event.title}</S.CardTitle>
-              <S.AvatarWrapper>
-                <S.CardAvatar size='md' assignee={event.assignee} />
-              </S.AvatarWrapper>
-              <S.CardBadge color={event.progress}>{event.progress}</S.CardBadge>
-            </S.CardWrapper>
-          </S.Cards>
-        );
-      })}
+      {isLoading ? (
+        <>
+          <S.CardSkeleton></S.CardSkeleton>
+          <S.CardSkeleton></S.CardSkeleton>
+          <S.CardSkeleton></S.CardSkeleton>
+          <S.CardSkeleton></S.CardSkeleton>
+          <S.CardSkeleton></S.CardSkeleton>
+        </>
+      ) : (
+        <>
+          {data.map((event: TaskOverviewProps) => {
+            return (
+              <S.Cards variant={'outline'} key={event.id}>
+                <S.CardWrapper>
+                  <S.CardTitle>{event.title}</S.CardTitle>
+                  <S.AvatarWrapper>
+                    <S.CardAvatar size="md" assignee={event.assignee} />
+                  </S.AvatarWrapper>
+                  <S.CardBadge color={event.progress}>{event.progress}</S.CardBadge>
+                </S.CardWrapper>
+              </S.Cards>
+            );
+          })}
+        </>
+      )}
     </S.OverViewContent>
   );
 }
