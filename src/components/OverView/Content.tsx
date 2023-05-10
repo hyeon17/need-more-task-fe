82ffc4078891f72b9c9265d812950ff8f71eca8a
hyeon17 <janghyeon8725@naver.com>
2023-05-10 20:21:30 +0900

    feat: 상세보기 내용 전역 상태 관리 데이터에 따른 삼항 연산자 추가

--- src/components/OverView/Content.tsx
+++ src/components/OverView/Content.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import * as S from '@/styles/overview.styles';
+import { useOverViewState } from '@/store/overViewStore';
 
 function Content({ content }: any) {
+  const { displayedData } = useOverViewState();
+  const data = displayedData ? displayedData : content;
   return (
     <S.OverViewContent>
-      {content.map((event: any) => {
-          return (
-            <S.Cards variant={'outline'} key={event.id}>
-              <S.CardWrapper>
-                <S.CardTitle>{event.title}</S.CardTitle>
-                <S.AvatarWrapper>
-                  <S.CardAvatar assignee={event.assignee} />
-                </S.AvatarWrapper>
-                <S.CardBadge color={event.progress}>{event.progress}</S.CardBadge>
-              </S.CardWrapper>
-            </S.Cards>
-          )
-        })
-      }
+      {data.map((event: any) => {
+        return (
+          <S.Cards variant={'outline'} key={event.id}>
+            <S.CardWrapper>
+              <S.CardTitle>{event.title}</S.CardTitle>
+              <S.AvatarWrapper>
+                <S.CardAvatar assignee={event.assignee} />
+              </S.AvatarWrapper>
+              <S.CardBadge color={event.progress}>{event.progress}</S.CardBadge>
+            </S.CardWrapper>
+          </S.Cards>
+        );
+      })}
     </S.OverViewContent>
   );
 }
