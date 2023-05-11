00cf6c049ce1ce2593fbd98abc8199953a6b1986
BeeMOre32 <windowssart01@gmail.com>
2023-05-11 21:15:05 +0900

    feat: 아바타 툴팁 추가

--- src/components/CommonAvatar/CommonAvatar.tsx
+++ src/components/CommonAvatar/CommonAvatar.tsx
@@ -1,12 +1,14 @@
-import { Avatar, AvatarGroup } from '@chakra-ui/react';
+import { Avatar, AvatarGroup, Tooltip } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { useModalState } from '@/store/modalStore';
 
 export interface IAssignee {
   profileImageUrl: string;
   userId: number;
+  name: string;
 }
 
 interface ICommonAvatar {
@@ -22,18 +24,28 @@ function CommonAvatar({
   max = 2,
   src = 'https://www.gravatar.com/avatar?d=mp&f=y',
 }: ICommonAvatar) {
+  const { onCloseModal } = useModalState();
   const router = useRouter();
+
+  const onClick = (userId: number) => {
+    onCloseModal();
+    router.push(`/profile/${userId}`);
+  };
+  console.log(assignee);
   return (
     <AvatarGroup size={size} max={max}>
       {assignee?.map((user: IAssignee) => {
         const { profileImageUrl, userId } = user;
         return (
-          <StyledAvatar
-            src={profileImageUrl ? profileImageUrl : src}
-            key={`userId${userId}`}
-            onClick={() => router.push(`/profile/${userId}`)}
-            style={{ cursor: 'pointer' }}
-          />
+          <Tooltip label={user.name}>
+            <StyledAvatar
+              src={profileImageUrl ? profileImageUrl : src}
+              key={`userId${userId}`}
+              onClick={() => onClick(userId)}
+              style={{ cursor: 'pointer' }}
+              size={size}
+            />
+          </Tooltip>
         );
       })}
     </AvatarGroup>
