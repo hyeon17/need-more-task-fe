092cf74b0d0f51a671427d9171e7e6f459934419
BeeMOre32 <windowssart01@gmail.com>
2023-05-11 13:15:17 +0900

    feat: modal action 에 assignee 함수 생성

--- src/components/modal/ModalActionAssignee.tsx
+++ src/components/modal/ModalActionAssignee.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ModalTaskActionSelectBox } from '@/styles/modal.styles';
 
 function ModalActionAssignee() {
+  const [assignee, setAssignee] = useState();
   return <ModalTaskActionSelectBox id="SET_STATUS" />;
 }
 
