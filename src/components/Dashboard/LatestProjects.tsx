1542c111741b41d38de7889f07afb3e98e976fb8
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-11 15:38:37 +0900

    feat: 대시보드 LatestProjects - 모든 Tasks, Task생성 라우터, 모달 추가

--- src/components/Dashboard/LatestProjects.tsx
+++ src/components/Dashboard/LatestProjects.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import * as D from '@/styles/dashboard.styles';
 import LatestProjectsList from '@/components/Dashboard/LatestProjectsList';
 import { Button } from '@chakra-ui/react';
+import { useModalState } from '@/store/modalStore';
+import { useRouter } from 'next/router';
 
 function LatestProjects() {
+  const router = useRouter();
+  const { onOpenCreate } = useModalState();
   return (
     <D.LatestProjectContainer>
       <D.DashboardH5>
         <div>
           <h5>Latest Projects</h5>
           <D.LatestProjectButtonWrapper>
-            <Button color="successColor" variant="outline">
+            <Button color="successColor" variant="outline" onClick={() => router.push('/task')}>
               모든 Tasks
             </Button>
-            <Button color="primary" variant="outline">
+            <Button color="primary" variant="outline" onClick={() => onOpenCreate()}>
               Task 생성
             </Button>
           </D.LatestProjectButtonWrapper>
