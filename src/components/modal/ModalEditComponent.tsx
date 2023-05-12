9ea222598d6bdd26109b3d1261a8d29d7236c625
BeeMOre32 <windowssart01@gmail.com>
2023-05-12 15:42:09 +0900

    clean: 사용하지 않았던 import문 최적화

--- src/components/modal/ModalEditComponent.tsx
+++ src/components/modal/ModalEditComponent.tsx
@@ -1,51 +1,9 @@
 import React from 'react';
-import { ModalActionComponentProps, ModalActionEditProps } from '@/type/componentProps';
+import { ModalActionEditProps } from '@/type/componentProps';
 import { Button, Input } from '@chakra-ui/react';
-import { ModalTaskActionSelectBox, ModalTaskDeleteButton } from '@/styles/modal.styles';
-import ModalActionAssignee from '@/components/modal/ModalActionAssignee';
+import { ModalTaskDeleteButton } from '@/styles/modal.styles';
 import { useMutation } from '@tanstack/react-query';
 import { deleteTask } from '@/apis/task';
-
-const setStatusConstants = [
-  {
-    key: 'SET_STATUS',
-    label: '해야할 일',
-    value: 'TODO',
-  },
-  {
-    key: 'SET_STATUS',
-    label: '진행중',
-    value: 'IN_PROGRESS',
-  },
-  {
-    key: 'SET_STATUS',
-    label: '완료',
-    value: 'DONE',
-  },
-];
-
-const setPriorityConstants = [
-  {
-    key: 'SET_PRIORITY',
-    label: '낮음',
-    value: 'LOW',
-  },
-  {
-    key: 'SET_PRIORITY',
-    label: '중간',
-    value: 'MEDIUM',
-  },
-  {
-    key: 'SET_PRIORITY',
-    label: '높음',
-    value: 'HIGH',
-  },
-  {
-    key: 'SET_PRIORITY',
-    label: '긴급',
-    value: 'URGENT',
-  },
-];
 
 function ModalEditComponent({ action, onEditMode }: ModalActionEditProps) {
   const { mutate } = useMutation(deleteTask, {
