e30a84bc3708bdc9cebe6d22921a65158229d91d
JangHyeonJun <janghyeon8725@naver.com>
2023-05-15 20:40:02 +0900

    feat: 데이터 전체 페이지 저장 상태 추가

--- src/store/overViewStore.ts
+++ src/store/overViewStore.ts
@@ -1,32 +1,28 @@
 import { create } from 'zustand';
 
 type OverViewState = {
-  TODO: string;
-  IN_PROGRESS: string;
-  DONE: string;
-  selectedProgress: string;
   displayedData: any;
   currentPage: number;
+  totalPage: number;
 };
 
 type OverViewAction = {
-  setSelectedProgress: (progress: string) => void;
   setDisplayedData: (data: any) => void;
   setCurrentPage: (page: number) => void;
+  setTotalPage: (page: number) => void;
+  getTotalPage: () => number;
 };
 
 type OverViewStore = OverViewState & OverViewAction;
 
 const overViewState = create<OverViewStore>((set, get) => ({
-  TODO: 'TODO',
-  IN_PROGRESS: 'IN_PROGRESS',
-  DONE: 'DONE',
-  selectedProgress: 'All' || get().TODO || get().IN_PROGRESS || get().DONE,
   displayedData: null,
   currentPage: 0,
-  setSelectedProgress: (progress: string) => set({ selectedProgress: progress }),
+  totalPage: 0,
   setDisplayedData: (data: any) => set({ displayedData: data }),
   setCurrentPage: (page: number) => set({ currentPage: page }),
+  setTotalPage: (page: number) => set({ totalPage: page }),
+  getTotalPage: () => get().totalPage,
 }));
 
 export const useOverViewState = () => overViewState((state) => state);
