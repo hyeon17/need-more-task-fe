643eddeb96454896d39158a9c19d24ed101f1a1e
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-15 22:36:03 +0900

    feat: Calendar - 미 로그인 유저 접근 제한, calendar - 컴포넌트 명 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,32 @@
 import Head from 'next/head';
 import Layout from '@/components/Layout';
 import CalendarPage from './calendar/index';
+import { useToast } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useUserInfo } from '@/store/userInfoStore';
+import { useAccessTokenStore } from '@/store/acceessTokenStore';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+  const { userInfo } = useUserInfo();
+  const { getAccessToken } = useAccessTokenStore();
+  const accessToken = getAccessToken();
+  const toast = useToast();
+
+  useEffect(() => {
+    if (!accessToken) {
+      toast({
+        title: '인증되지 않은 사용자는 접근할 수 없습니다.',
+        description: '로그인 페이지로 이동합니다.',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+      router.push('/login');
+    }
+  }, [accessToken]);
+
   return (
     <>
       <Head>
